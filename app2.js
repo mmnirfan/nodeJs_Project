1ed1c096e25dc02bccdc7002548ec8ac6fe90891
mmnirfan <46536829+mmnirfan@users.noreply.github.com>
2020-12-07 17:49:26 +0530

    Create app2.js
    new file for Vendor server

---
+++ app2.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const url = "mongodb://localhost/nodeJs_newDBex"
+
+const app = express();
+
+
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
+
+con.on('open', function(){
+    console.log('connected...')
+})
+
+app.use(express.json());
+
+const vendorRouter = require('./routes/vendors')
+app.use('/vendors', vendorRouter);
+
+app.listen(9000, function(){
+    console.log('Server started');
+});
